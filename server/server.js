7cd052aeb9bf9fdf72530be28a191ae576a008bc
strivendra002 <110083471+strivendra002@users.noreply.github.com>
2025-02-28 18:07:01 +0530

    backend

--- server/server.js
+++ server/server.js
@@ -17,6 +17,12 @@ const port = process.env.PORT || 5000;
 const path = require("path");
 __dirname = path.resolve();
 
+// if (process.env.NODE_ENV === "production") {
+//   app.use(express.static(path.join(__dirname, "client" , "build")));
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+//   });   
+// } 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "..", "client", "build"))); // Move up one directory
   app.get("*", (req, res) => {
